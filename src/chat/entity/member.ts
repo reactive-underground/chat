1b8f7cf6e6ce2308d8ad92d5fc1576710ad8084b
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-01 05:17:01 +0600

    implements chat member

--- src/chat/entity/member.ts
+++ src/chat/entity/member.ts
@@ -1,1 +1,11 @@
-export class Member {}
+export class Member {
+    private readonly id: string;
+
+    constructor(id: string) {
+        this.id = id;
+    }
+
+    public getId(): string {
+        return this.id;
+    }
+}
