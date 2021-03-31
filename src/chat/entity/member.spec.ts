220a4d3b7608e323361b8397cc4cc0189d7ce407
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-01 05:55:30 +0600

    test: member isEqualTo

--- src/chat/entity/member.spec.ts
+++ src/chat/entity/member.spec.ts
@@ -9,4 +9,22 @@ describe('Member', () => {
     expect(member).toBeDefined();
     expect(member.getId()).toEqual(id);
   });
+
+  describe("isEqualTo", () => {
+    it("should be equal", () => {
+      const id = new Id("0000-000-1");
+      const member = new Member(id);
+      const member2 = new Member(id);
+
+      expect(member.isEqualTo(member2)).toBeTruthy();
+    });
+
+    it("should be not equal", () => {
+      const id = new Id("0000-000-1");
+      const member = new Member(id);
+      const member2 = new Member(new Id("0000-000-2"));
+
+      expect(member.isEqualTo(member2)).toBeFalsy();
+    });
+  });
 });
