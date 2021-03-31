49e5dfa5eb93e8280471e2dea9c2ca1bd180a00e
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-01 05:23:27 +0600

    impl: chat id

--- src/chat/entity/chat.ts
+++ src/chat/entity/chat.ts
@@ -2,10 +2,16 @@ import { Member } from "./member";
 
 export class Chat {
 
+    private readonly id: string;
     private readonly members: Member[];
 
-    constructor(members: Member[]) {
+    constructor(id: string, members: Member[]) {
+        this.id = id;
         this.members = members;
+    }
+
+    public getId(): string {
+        return this.id;
     }
 
     public getMembers(): Member[] {
