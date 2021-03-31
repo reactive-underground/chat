c1d53150b5fcefbd60c98b10d363d2ddfb54bd2b
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-01 05:11:25 +0600

    implement chat members as numbers

--- src/chat/entity/chat.ts
+++ src/chat/entity/chat.ts
@@ -1,1 +1,12 @@
-export class Chat {}
+export class Chat {
+
+    private readonly members: number[];
+
+    constructor(members: number[]) {
+        this.members = members;
+    }
+
+    public getMembers(): number[] {
+        return this.members;
+    }
+}
