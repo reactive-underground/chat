fe9df23ad1fd2813d2d12fc067033de9a05c0450
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-01 05:21:51 +0600

    test: chat id

--- src/chat/entity/chat.spec.ts
+++ src/chat/entity/chat.spec.ts
@@ -2,15 +2,17 @@ import { Chat } from './chat';
 import { Member } from "./member";
 
 describe('Chat', () => {
-  it('should be defined', () => {
-    const members = [
-        new Member("1"),
-        new Member("2"),
-        new Member("3")
-    ];
-    const chat = new Chat(members);
+    it('should be defined', () => {
+        const members = [
+            new Member("1"),
+            new Member("2"),
+            new Member("3")
+        ];
+        const id = "000-2"
+        const chat = new Chat(id, members);
 
-    expect(chat).toBeDefined();
-    expect(chat.getMembers()).toEqual(members);
-  });
+        expect(chat).toBeDefined();
+        expect(chat.getId()).toBe(id);
+        expect(chat.getMembers()).toEqual(members);
+    });
 });
