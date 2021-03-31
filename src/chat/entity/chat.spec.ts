6ddd150937f0ac528f1241274a5f327f5dbab622
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-01 05:09:27 +0600

    test for chat members as numbers

--- src/chat/entity/chat.spec.ts
+++ src/chat/entity/chat.spec.ts
@@ -2,8 +2,10 @@ import { Chat } from './chat';
 
 describe('Chat', () => {
   it('should be defined', () => {
-    const chat = new Chat();
+    const members = [1,2,3];
+    const chat = new Chat(members);
 
     expect(chat).toBeDefined();
+    expect(chat.getMembers()).toEqual(members);
   });
 });
