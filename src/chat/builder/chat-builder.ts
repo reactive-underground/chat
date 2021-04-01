ef5dcbc7235462158faaa77e863dbbfa7abd6f4c
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-02 01:14:13 +0600

    impl: chat builder

--- src/chat/builder/chat-builder.ts
+++ src/chat/builder/chat-builder.ts
@@ -1,1 +1,11 @@
-export class ChatBuilder {}
+import { Chat } from "../entity/chat";
+import { Id } from "../entity/id";
+
+export class ChatBuilder {
+    public build(): Chat {
+        return new Chat(
+            new Id("000-1"),
+            []
+        );
+    }
+}
