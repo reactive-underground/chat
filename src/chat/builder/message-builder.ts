6b99f9f7f86d04602fa9a093d156a205e0a1194c
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-02 01:34:30 +0600

    impl: MessageBuilder.build

--- src/chat/builder/message-builder.ts
+++ src/chat/builder/message-builder.ts
@@ -1,1 +1,16 @@
-export class MessageBuilder {}
+import { Message } from "../entity/message";
+import { Id } from "../entity/id";
+import { Member } from "../entity/member";
+
+export class MessageBuilder {
+    public build(): Message {
+        return new Message(
+            new Id("123"),
+            "Default message",
+            new Member(
+                new Id("222")
+            ),
+            new Date("2000-01-01")
+        );
+    }
+}
