89c521ff7c24974e44fafd4ceae674d9918a69a1
Artem Ilinykh <devsinglesly@gmail.com>
2021-04-02 01:06:20 +0600

    impl: create message

--- src/chat/entity/message.spec.ts
+++ src/chat/entity/message.spec.ts
@@ -15,7 +15,7 @@ describe('Message', () => {
     expect(message).toBeDefined();
     expect(message.getId()).toEqual(id);
     expect(message.getText()).toEqual(text);
-    expect(message.getMember()).toEqual(member);
+    expect(message.getSender()).toEqual(member);
     expect(message.getDate()).toEqual(date);
   });
 });
--- src/chat/entity/message.ts
+++ src/chat/entity/message.ts
@@ -1,1 +1,33 @@
-export class Message {}
+import { Id } from "./id";
+import { Member } from "./member";
+
+export class Message {
+
+    private readonly id: Id;
+    private readonly text: string;
+    private readonly sender: Member;
+    private readonly date: Date;
+
+    constructor(id: Id, text: string, sender: Member, date: Date) {
+        this.id = id;
+        this.text = text;
+        this.sender = sender;
+        this.date = date;
+    }
+
+    public getId(): Id {
+        return this.id;
+    }
+
+    public getText(): string {
+        return this.text;
+    }
+
+    public getSender(): Member {
+        return this.sender;
+    }
+
+    public getDate(): Date {
+        return this.date;
+    }
+}
